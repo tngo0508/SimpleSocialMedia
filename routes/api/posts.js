5f29f646848207b86c3d120786f51bd17d2ec51f
Thomas Ngo <tngo0508@gmai.com>
2020-07-06 20:00:14 -0700

    Get & delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,8 @@ const passport = require("passport");
 
 // Post model
 const Post = require("../../models/Post");
+// Profile model
+const Profile = require("../../models/Profile");
 
 // Validation
 const validatePostInput = require("../../validation/post");
@@ -58,6 +60,35 @@ router.post(
     });
 
     newPost.save().then((post) => res.json(post));
+  }
+);
+
+// @route  DELETE api/posts/:id
+// @desc   Delete post
+// @access Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          // Delete
+          post.remove().then(() => {
+            res.json({ success: true });
+          });
+        })
+        .catch((err) =>
+          res.status(404).json({ postnotfound: "No post found" })
+        );
+    });
   }
 );
 
