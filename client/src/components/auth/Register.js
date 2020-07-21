6c45bd893fe0a6d5bdf561a6318eb4ba94228f9b
Thomas Ngo <tngo0508@gmai.com>
2020-07-20 21:38:30 -0700

    redux login action & set current user

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import classnames from "classnames";
 import { connect } from "react-redux";
-import { registerUser } from "../../actions/authAction";
+import { registerUser } from "../../actions/authActions";
 
 class Register extends Component {
   constructor() {
